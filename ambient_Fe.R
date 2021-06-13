fcf0d8ed789ed8c092a994b96cf11e04cbba720e
tleung2 <tleung02@gmail.com>
2021-06-13 00:33:55 -0500

    kmeans

--- ambient_Fe.R
+++ ambient_Fe.R
@@ -56,7 +56,7 @@ chase_Fe2<-chase_Fe %>%
                     "Crandall's Beach", "Lake Wapello") & Fe > 0)
 
 
-   ### make boxplots to look at the variance
+   ### ----  1)  Boxplots of DFe from 130 lakes  --------
    ### plot by order of median using reorder() and FUN = median
 ambient_avgFe %>%
   na.omit() %>%
@@ -79,29 +79,61 @@ ggplot(aes(y = mean, x = reorder(Waterbody2, mean, FUN = median, .desc = FALSE, 
         legend.text = element_text(size = 16),
         legend.title = element_blank())
 
+   ### ----  2)  Boxplots of DFe from subset 30 lakes  --------
+all.2018 %>%
+  ggplot(aes(x = avg_Fe)) +
+  geom_boxplot(aes(fill = "gray"), na.rm = TRUE) +
+  #stat_summary(fun.y=mean, geom="point", shape=4, size=2) +
+  scale_fill_brewer(palette = "BrBG") +
+  labs(x = expression(paste('Total Dissolved Fe (', mu, 'mol/L)'))) +
+  #labs(x = 'Dissolved organic carbon (mg/L') +
+  #coord_flip() +
+  facet_wrap(.~Month, ncol = 1) +
+  theme(panel.background = element_blank(),
+        axis.line.x = element_line(color = "black"),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.text.x = element_text(size = 16, color = "black", hjust = 1, vjust = 0.5),
+        axis.title.x = element_text(size = 16, color = "black"),
+        axis.title.y = element_blank(),
+        strip.text = element_text(size = 16),
+        legend.position = "N/A",
+        legend.key = element_blank(),
+        legend.text = element_text(size = 16),
+        legend.title = element_blank())
+
 
 #######################################################################
    #################    DESCRIPTIVE STATISTICS   #################
+
+   ### ----  1) Summarise Fe in 130 lakes  ----------
 ambient_avgFe %>%
   subset(Waterbody2 == "Artificial Lakes") %>%
   summarise(quant = quantile(TDFe, probs = c(0.25,0.5,0.75)),
             mean = mean(TDFe),
             min = min(TDFe))
              
-summary(all.2018$avg_Fe)
-avg.fe.2018<-all.2018 %>%
+   ### ----  2) Summarise subset of 30 lakes   ----------
+avg.doc.2018<-all.2018 %>%
   na.omit() %>%
 #ambient_avgFe <- ambient_Fe %>%
-  select(c(4,5,7,8,13)) %>%
+  select(c(7,19)) %>%
   group_by(Site2) %>%
   #mutate(mean = mean(avg_Fe))#%>%
   #ungroup()
-  summarise(mean = mean(avg_Fe))
+  summarise(doc = mean(DOC))
 write.csv(ambient_avgFe, "ambient_avgFe.csv")
 
-ambient_avgFe %>%
-  subset(Waterbody2 == "Artificial Lakes") %>%
-  count()
+all_2018 %>%
+  subset(Week %in% c(1:15)) %>%
+  na.omit() %>%
+  group_by(Month) %>%
+  summarise(quant = quantile(avg_Fe, probs = c(0.25,0.5,0.75)),
+            mean = mean(avg_Fe),
+            min = min(avg_Fe),
+            max = max(avg_Fe))
+ 
+sum(avg.doc.2018$doc < 6.67)
 
 #######################################################################
    #################    CORRELATION ANALYSIS     #################
@@ -171,8 +203,8 @@ pivot.2018b %>%
    ### Pivot wider 2018 Fe data so each lake is a column
 pivot.2018<-all.2018 %>%
   na.omit() %>%
-  select(c(4,7,13)) %>%
-  pivot_wider(names_from = "Date", values_from = "avg_Fe") 
+  select(c(5,7,13)) %>%
+  pivot_wider(names_from = "Week", values_from = "avg_Fe") 
 
 
    ### Spearman correlation matrix
@@ -274,20 +306,9 @@ write.csv(pair.res$p.value, "pairwise_results_2018.csv")
 wilcox.test(ambient_avgFe$mean~ ambient_avgFe$Waterbody2)
 kruskal.test(lakes_fe$TDFe, lakes_fe$landform, correct=FALSE, na.rm = TRUE)
 
-#######################################################################
-   ###################    LINEAR REGRESSION   #####################
 
-mob1<-lm(avg_Fe~pH+Temperature+DO+DOC+Turbidity, data = all.2018)
-summary(mob1)
-
-   ### linear mixed model
-mixed.lmer <- lmer(avg_Fe ~ DOC + (1|Waterbody2), data = all.2018)
-summary(mixed.lmer)
-
-   ### Linear regression for week 1 to 8
-wk1_8<-all.2018 %>%
-  na.omit() %>%
-  subset(Week >= 1 & Week <=8)
+#########################################################################
+   #########################   CLUSTERING   #########################
 
    ### Plot Linear Regression: Fe over time
    ### Smooth moethod = loess means locally weighted regression
@@ -306,7 +327,7 @@ all.2018$Site2<-factor(all.2018$Site2,
 
 all.2018 %>%
   subset(!Site %in% c("Denison", "McIntosh Woods", "North Twin Lake West")) %>%
-ggplot(aes(x=Date, y=avg_Fe)) + 
+  ggplot(aes(x=Date, y=avg_Fe)) + 
   geom_point(colour="black", size = 2) + 
   stat_smooth(method = 'loess', aes(color = 'linear'), se = TRUE, formula = y ~ x) + ## Turns on confidence intervals
   #stat_poly_eq(aes(label = ..eq.label..), formula = y ~ x, parse = TRUE, size = 3) +                                 ## Turns on equation
@@ -316,19 +337,142 @@ ggplot(aes(x=Date, y=avg_Fe)) +
   #scale_y_continuous(position = "right") +  ## places y scale on right
   facet_wrap(~Site2, scales = "free_y", ncol = 5) +
   theme_classic() +
-  theme(axis.text.y.left = element_text(size=11, color = "black"), 
-        axis.text.x.bottom = element_text(size=11, color = "black"),
-        axis.title.x = element_text(size=11),
-        axis.title.y = element_text(size=11),
-        strip.text = element_text(size = 11))
+  theme(axis.text.y.left = element_text(size=12, color = "black"), 
+        axis.text.x.bottom = element_text(size=12, color = "black"),
+        axis.title.x = element_blank(),
+        axis.title.y = element_text(size=12),
+        strip.text = element_text(size = 12))
 
-clusters2 <- hclust(dist(pivot.2018[, 2:16]), method = "single")
+clusters2 <- hclust(dist(pivot.2018[, 2:16]), method = "complete")
 plot(clusters2)
-   ###########  Plot Linear Regression: Slope vs Volume  #############
+
+   ### -----  1)  optimal number of k clusters "elbow method"  -------
+   ### First omit an Null values
+pivot.2018b<-pivot.2018%>%
+  select(c(1,4,6:16)) %>%
+  na.omit()
+   ### Find k group
+fviz_nbclust(pivot.2018b[,c(2:13)], kmeans, method = "wss")
+
+   ### ----   2)  Run k-means cluster analysis   ----------
+clust.res <- kmeans(pivot.2018b[,2:13], 3, nstart = 30)
+print(clust.res)
+   
+   ### ----  3)  Plot kmeans results  ----------
+   ### Plot using default function
+fviz_cluster(clust.res, data = pivot.2018b[,c(2:13)]) +
+  scale_fill_brewer(palette = "Set2") +
+  theme(panel.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.text = element_text(size = 13, color = "black"),
+        axis.title.y = element_text(size = 13, color = "black"),
+        axis.title.x = element_blank(),
+        strip.text = element_text(size = 13, color = "black"),
+        axis.line = element_line(color = "black"))
+
+
+   ### Plotting kmeans with ggplot
+trythis<-stats::prcomp(pivot.2018b[,c(2:13)], scale = FALSE, center = FALSE)
+state_scores<-as.data.frame(scores(trythis))
+state_scores$cluster <- clust.res$cluster
+state_scores$state <- pivot.2018b$Site2
+head(state_scores)
+
+chull(state_scores %>% filter(cluster ==1) %>% select(PC1, PC2))
+
+grp.1 <- state_scores[state_scores$cluster == 1, ][chull(state_scores %>% filter(cluster ==1) %>% select(PC1, PC2) ), ]  # hull values for cluster 1
+grp.2 <- state_scores[state_scores$cluster == 2, ][chull(state_scores %>% filter(cluster ==2) %>% select(PC1, PC2) ), ]  # hull values for cluster 2
+grp.3 <- state_scores[state_scores$cluster == 3, ][chull(state_scores %>% filter(cluster ==3) %>% select(PC1, PC2) ), ]  # hull values for cluster 3
+all_hulls <- rbind(grp.1,grp.2,grp.3)
+head(all_hulls)
+
+ggplot(data = state_scores) + 
+  geom_point(aes(x = PC1, y = PC2, color = as.factor(cluster)),size = 3) +
+  geom_text(aes(x = PC1, y = PC2, color = as.factor(cluster), 
+                label = state),size = 5,  hjust = -0.1, vjust = 0.5)  +
+  geom_polygon(data = all_hulls, 
+               aes(x = PC1, y = PC2, fill = as.factor(cluster),  #as.factor(cluster)
+                   colour =  NA), alpha = 0.25) + 
+                   #colour =  as.factor(cluster)), alpha = 0.25) + 
+  scale_fill_brewer(palette = "Set2") +
+  scale_color_brewer(palette = "Set2") +
+  theme(panel.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.text = element_text(size = 12, color = "black"),
+        axis.title.y = element_text(size = 12, color = "black"),
+        axis.title.x = element_text(size = 12, color = "black"),
+        strip.text = element_text(size = 12, color = "black"),
+        axis.line = element_line(color = "black"),
+        legend.position = "none")
+
+
+   ### ---- 4)  Re-cluster k-means output by group  ----------
+pivot.group2<-group2 %>%
+  na.omit() %>%
+  select(c(5,7,13)) %>%
+  pivot_wider(names_from = "Week", values_from = "avg_Fe") 
+fviz_nbclust(pivot.group2[,c(4,6:16)], kmeans, method = "wss")
+
+clust.res2 <- kmeans(pivot.group2[,c(4,6:16)], 3, nstart = 30)
+print(clust.res2)
+fviz_cluster(clust.res2, data = pivot.group2[,c(4,6:16)])
+#######################################################################
+   ###################    LINEAR MIXED MODEL   ###################
+
+   ### ---- 1) Create groups based on k-means results ------
+group1<-subset(all.2018, Site2 %in% c("Backbone", "Beeds Lake",
+                                      "Lower Pine Lake", "West Okoboji",
+                                      "Clear Lake"))
+group2<-subset(all.2018, Site2 %in% c("Green Valley", "George Wyth",
+                                      "Brushy Creek","Big Creek", "Big Spirit",
+                                      "Black Hawk","North Twin","Springbrook",
+                                      "Blue Lake","Lake Manawa", "Union Grove",
+                                      "Prairie Rose","Nine Eagles","Lake Ahquabi",
+                                      "Rock Creek","Lacey Keosauqua"))
+group3<-subset(all.2018, Site2 %in% c("Lake Macbride","Lake Anita",
+                                      "Lake Wapello", "Rathburn Lake",
+                                      "Red Haw","Lake Keomah","Lake Darling",
+                                      "Lake of Three Fires",
+                                      "Viking Lake"))
+
+   ### ---- 2) Run Linear Mixed Model Analysis --------
+mob1<-lm(avg_Fe~pH+Temperature+DO+DOC+Turbidity, data = group1)
+summary(mob1)
+
+mob2<-lm(avg_Fe~pH+Temperature+DO+DOC+Turbidity, data = group2)
+summary(mob2)
+
+mob3<-lm(avg_Fe~pH+Temperature+DO+DOC+Turbidity, data = group3)
+summary(mob3)
+
+   ### linear mixed model
+group1<-na.omit(group1)
+mixed.lmer1 <- lmer(avg_Fe ~ Week + (1|DOC)+(1|Turbidity)+(1|DO) +(1|pH)+(1|Temperature), data = group1)
+summary(mixed.lmer1)
+
+mixed.lmer2 <- lmer(avg_Fe ~ DOC + pH + Temperature + DO + Turbidity + (1|DOC), data = group2)
+summary(mixed.lmer2)
+
+mixed.lmer3 <- lmer(avg_Fe ~ DOC + pH + Temperature + DO + Turbidity + (1|Week), data = group3)
+summary(mixed.lmer3)
+
+   ### Linear regression for week 1 to 8
+wk1_8<-all.2018 %>%
+  na.omit() %>%
+  subset(Week >= 1 & Week <=8)
+mixed.lmer2
+
+#######################################################################
+###################    LINEAR REGRESSION   #####################
+
+
+   ### ----- 1)  Plot Linear Regression: Fe vs slope----------
    ### LR of Week vs Fe show decline Fe from Week 1-8
    ### Pulled slope from this LR 
    ### Run LR for slope vs lake volume
-   
+
 chase_slope %>%
   #subset(!Site %in% c("Denison", "McIntosh Woods", "North Twin Lake West")) %>%
   ggplot(aes(x=reorder(Site2, slope), y=slope)) + 
@@ -347,29 +491,30 @@ chase_slope %>%
         axis.title.x = element_blank(),
         axis.title.y = element_text(size=12),
         strip.text = element_text(size = 12))
+   
 
-
-   ###########  Plot Linear Regression: Fe vs. DOC  #############
+   ### ---- 2)  Plot Linear Regression: Fe vs. DOC  -----------
    ### This is for browning....
 
-all.2018 %>%
-  subset(!Site %in% c("Denison", "McIntosh Woods", "North Twin Lake West")) %>%
-  ggplot(aes(x=DOC, y=avg_Fe)) + 
+group3 %>%
+  #subset(!Site %in% c("Denison", "McIntosh Woods", "North Twin Lake West")) %>%
+  ggplot(aes(y=DOC, x=Fe)) + 
   geom_point(stat="identity") +
   #geom_text(stat = "identity", label = round(chase_slope$volume,0), nudge_y = -0.1) +
-  #stat_smooth(method = 'loess', aes(color = 'linear'), se = TRUE, formula = y ~ x) + ## Turns on confidence intervals
+  stat_smooth(method = 'lm', aes(color = 'linear'), se = TRUE, formula = y ~ x) + ## Turns on confidence intervals
   #stat_poly_eq(aes(label = ..eq.label..), formula = y ~ x, parse = TRUE, size = 3) +                                 ## Turns on equation
-  #stat_cor(label.x.npc = "center", label.y.npc = "top", size = 3) + ## Turns on r value
-  labs(y = expression(paste('Dissolved Fe (', mu, 'g/L) per week')),
-       x = 'Dissolved organic carbon (mg/L)') +
+  stat_cor(label.x.npc = "center", label.y.npc = "top", size = 4) + ## Turns on r value
+  labs(x = expression(paste('Dissolved Fe (', mu, 'g/L) per week')),
+       y = 'Dissolved organic carbon (mg/L)') +
   #scale_y_continuous(position = "right") +  ## places y scale on right
-  #facet_wrap(~Site2, scales = "free", ncol = 5) +
-  theme_classic() +
-  theme(axis.text.y = element_text(size=12, color = "black"), 
-        axis.text.x = element_text(size=12, color = "black"),
-        axis.title.x = element_text(size = 12),
-        axis.title.y = element_text(size=12),
-        strip.text = element_text(size = 12))
+  facet_wrap(~Site2, scales = "free", ncol = 3) +
+  theme(panel.background = element_blank(),
+        axis.line = element_line(color = "black"),
+        axis.text.y = element_text(size=14, color = "black"), 
+        axis.text.x = element_text(size=14, color = "black"),
+        axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size=14),
+        strip.text = element_text(size = 14))
 
    ##############   PLOT: Fe Variability West Okoboji   ##############
 
